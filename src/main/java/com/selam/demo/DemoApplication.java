76e31e2b1e52a7748cbac054bbf996ccc1899daa
selamawitsh <selamshimeles9@gmail.com>
2026-04-29 16:30:58 +0300

    update demoApplication to ecommerce package

--- src/main/java/com/selam/demo/DemoApplication.java
+++ src/main/java/com/selam/demo/DemoApplication.java
@@ -1,13 +1,29 @@
 package com.selam.demo;
 
+import com.selam.demo.ecommerce.CheckoutService;
+import com.selam.demo.ecommerce.Order;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		ConfigurableApplicationContext context =
+				SpringApplication.run(DemoApplication.class, args);
+
+		// Get the fully assembled CheckoutService
+		CheckoutService checkout = context.getBean(CheckoutService.class);
+
+		// Create an order
+		Order order1 = new Order("ORD-001", "Selam", "selam@email.com", 49.99);
+		checkout.checkout(order1);
+
+		Order order2 = new Order("ORD-002", "John", "john@email.com", 129.50);
+		checkout.checkout(order2);
+
+		context.close();
 	}
 
 }
